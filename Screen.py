728129db54c186863ba5f9046f994c03f602081f
devanegas <dieguitovan@gmail.com>
2019-02-07 01:35:50 -0700

    Worksgit add .!

--- Screen.py
+++ Screen.py
@@ -1,19 +1,20 @@
 import pygame 
 import math
+import random
 from random import sample
 
 
 global colors
 pygame.font.init()
 font = pygame.font.SysFont("Times New Roman", 30)
-colors = font.render(" 0", True, (0,0,0)), font.render(" 1", True, (0,0,0)), font.render(" 2", True, (0,0,0)), font.render(" 3", True, (0,0,0)), font.render(" 4", True, (0,0,0)), font.render(" 5", True, (0,0,0)), font.render(" 6", True, (0,0,0)), font.render(" 7", True, (0,0,0)), font.render(" 8", True, (0,0,0)), font.render(" B", True, (0,0,0))
+colors = font.render(" 0", True, (0,128,0)), font.render(" 1", True, (0,0,255)), font.render(" 2", True, (0,128,0)), font.render(" 3", True, (200,0,0)), font.render(" 4", True, (0,0,0)), font.render(" 5", True, (0,0,0)), font.render(" 6", True, (0,0,0)), font.render(" 7", True, (0,0,0)), font.render(" 8", True, (0,0,0)), font.render(" B", True, (255,0,0))
 
 class cell:
     def __init__(self, x, y):
         self.rect = pygame.Rect(35*x, 35*y, 35, 35)
         self.isClicked = False
         self.isBomb = False
-        self.color = font.render("", True, (0,128,0))
+        self.color = font.render("", True, (0,128,128))
         self.connections = 0
 
     """def countBombs(self, pos):
@@ -47,98 +48,106 @@ def main(xSize, ySize, nBombs):
     pygame.init()
 
     squareSize = 35
-    height = squareSize * ySize
-    width = squareSize * xSize
+    height = squareSize * (ySize+2)
+    width = squareSize * (xSize+2)
 
 
     screen = pygame.display.set_mode((height, width))
-    squares = [[cell(x, y) for y in range(0, ySize)] for x in range(0, xSize)]
+    squares = [[cell(x, y) for y in range(ySize+2)] for x in range(xSize+2)]
 
 
-    for square in sample([i for j in squares for i in j], nBombs):
-        square.isBomb = True
+    #for square in sample([i for j in squares for i in j], nBombs):
+    #   square.isBomb = True"""
 
+    bombsPlaced = 0
+    done = False
+    while(not done):
+        xRand = random.randint(1, xSize)
+        yRand = random.randint(1, ySize)
+        if squares[xRand][yRand].isBomb == True:
+            done = False
+        else:
+            squares[xRand][yRand].isBomb = True
+            print(xRand, yRand)
+            bombsPlaced += 1
 
-    for x in range(0, xSize):
-        for y in range(0, ySize):
+        if bombsPlaced == nBombs:
+            done = True
+
+    for x in range(1, xSize+1):
+        for y in range(1, ySize+1):
             if squares[x][y].isBomb:
-                if x-1 <0 and y-1 <0:
-                    squares[x+1][y].connections += 1
-                    squares[x+1][y+1].connections += 1
-                    squares[x][y+1].connections += 1
-                elif x+1 >= xSize and y-1 < 0:
-                    squares[x-1][y].connections += 1
-                    squares[x-1][y+1].connections += 1
-                    squares[x][y+1].connections += 1
-                elif x+1 >= xSize and y+1 >= ySize:
-                    squares[x-1][y].connections += 1
-                    squares[x-1][y-1].connections += 1
-                    squares[x][y-1].connections += 1
-                elif x-1 < 0 and y+1 >= ySize:
-                    squares[x+1][y].connections += 1
-                    squares[x+1][y-1].connections += 1
-                    squares[x][y-1].connections += 1
-
-            #Left Arista
-                elif x == 0 and y-1 >= 0 and y+1 < ySize:
-                    squares[x+1][y].connections += 1
-                    squares[x+1][y+1].connections += 1
-                    squares[x][y+1].connections += 1
-                    squares[x][y-1].connections += 1
-                    squares[x+1][y-1].connections += 1
-            #Top Arista
-                elif x+1 < xSize and y == 0 and x-1 >= 0:
-                    squares[x-1][y].connections += 1
-                    squares[x-1][y+1].connections += 1
-                    squares[x][y+1].connections += 1
-                    squares[x+1][y].connections += 1
-                    squares[x+1][y+1].connections += 1
-            #Right Arista
-                elif x == xSize-1 and y+1 < ySize and y-1 >= 0:
-                    squares[x-1][y].connections += 1
-                    squares[x-1][y-1].connections += 1
-                    squares[x][y-1].connections += 1
-                    squares[x-1][y-1].connections += 1
-                    squares[x-1][y+1].connections += 1
-
-            #Down Arista
-                elif x-1 >= 0 and y == ySize-1 and x+1 < xSize:
-                    squares[x+1][y].connections += 1
-                    squares[x+1][y-1].connections += 1
-                    squares[x][y-1].connections += 1
-                    squares[x-1][y].connections += 1
-                    squares[x-1][y-1].connections += 1
+                squares[x-1][y-1].connections += 1
+                squares[x][y-1].connections += 1
+                squares[x+1][y-1].connections += 1
+                squares[x-1][y].connections += 1
+                squares[x+1][y].connections += 1
+                squares[x-1][y+1].connections += 1
+                squares[x][y+1].connections += 1
+                squares[x+1][y+1].connections += 1
                 
-                elif x+1 < xSize and y+1 <ySize and x-1 >= 0 and y-1 >=0:
-                    print(x,y)
-                    squares[x-1][y-1].connections += 1
-                    squares[x][y-1].connections += 1
-                    squares[x+1][y-1].connections += 1
-                    squares[x-1][y].connections += 1
-                    squares[x+1][y].connections += 1
-                    squares[x-1][y+1].connections += 1
-                    squares[x][y+1].connections += 1
-                    squares[x+1][y+1].connections += 1
+
+
+    def reveal(x, y):
+        if x <= xSize and y <= ySize and x > 0 and y > 0 and squares[x][y].isBomb == False and squares[x][y].isClicked == False:
+
+            squares[x-1][y-1].color = colors[squares[x-1][y-1].connections]
+            squares[x][y-1].color = colors[squares[x][y-1].connections]
+            squares[x+1][y-1].color = colors[squares[x+1][y-1].connections]
+            squares[x-1][y].color = colors[squares[x-1][y].connections]
+            squares[x+1][y].color = colors[squares[x+1][y].connections]
+            squares[x-1][y+1].color = colors[squares[x-1][y+1].connections]
+            squares[x][y+1].color = colors[squares[x][y+1].connections]
+            squares[x+1][y+1].color = colors[squares[x+1][y+1].connections]
+
+            squares[x][y].isClicked = True
+
+
+            if squares[x-1][y-1].connections == 0:
+                reveal(x-1, y-1)
+            if squares[x][y-1].connections == 0:
+                reveal(x, y-1)
+            if squares[x+1][y-1].connections == 0:
+                reveal(x+1, y-1)
+            if squares[x-1][y].connections == 0:
+                reveal(x-1, y)
+            if squares[x+1][y].connections == 0:
+                reveal(x+1, y)
+            if squares[x-1][y+1].connections == 0:
+                reveal(x-1, y+1)
+            if squares[x][y+1].connections == 0:
+                reveal(x, y+1)
+            if squares[x+1][y+1].connections == 0:
+                reveal(x+1, y+1)
+        else:
+            return
+
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                    for x in range(0, xSize):
-                        for y in range(0, ySize):
-                            squares[x][y].click(event.pos, (75, 77, 79))
+                xCor, yCor = event.pos
+                xReal = math.floor(xCor/35)
+                yReal = math.floor(yCor/35)
+                if squares[xReal][yReal].connections == 0 and xReal <= xSize and yReal <= ySize and xReal > 0 and yReal > 0 and squares[xReal][yReal].isBomb == False:
+                    reveal(xReal, yReal)
+                for x in range(1, xSize+1):
+                    for y in range(1, ySize+1):
+                        squares[x][y].click(event.pos, (75, 77, 79))
+
                             #print(squares[x][y].connections)
+                
 
-                        
-
-                            
+                     
             screen.fill((141, 144, 147))
 
-            for x in range(0, xSize):
-                for y in range (0, ySize):
+            for x in range(1, xSize+1):
+                for y in range (1, ySize+1):
                     squares[x][y].draw(screen)
             
             pygame.display.flip()
 
-main(16, 16, 40)
+main(6, 6, 5)
